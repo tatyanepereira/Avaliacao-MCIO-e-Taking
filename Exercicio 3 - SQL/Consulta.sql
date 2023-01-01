dab52f901da3ff08a8044f7badddf09755d888be
Tatiane Pereira <106749928+tatyanepereira@users.noreply.github.com>
2023-01-01 11:41:10 -0300

    Update Consulta.sql

--- Exercicio 3 - SQL/Consulta.sql
+++ Exercicio 3 - SQL/Consulta.sql
@@ -1,2 +1,16 @@
-SELECT * FROM Pessoa;
-SELECT * FROM Endereco;
+SELECT
+      Pessoa.Pessoa_id
+      ,Pessoa.Nome
+      ,Pessoa.Cpf
+      ,Pessoa.Idade
+      ,Pessoa.Sexo
+      ,Endereco.Tipo
+      ,Endereco.Logradouro
+      ,Endereco.Numero
+      ,Endereco.Complemento
+      ,Endereco.Cep
+      ,Endereco.Cidade
+      ,Endereco.Estado
+FROM
+      Pessoa
+      inner join Endereco on Pessoa.Pessoa_id = Endereco.Pessoa_id;
